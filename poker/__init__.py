7f34e1332e4a449e6b510016ba67e2cec24f6ad8
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-05-07 15:39:39 +0530

    deal

--- poker/__init__.py
+++ poker/__init__.py
@@ -1,7 +1,7 @@
 import torch
 
 SUITS = ['S', 'H', 'D', 'C']
-RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', "K', 'A"]
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A']
 
 assert len(SUITS) == 4
 assert len(RANKS) == 13
@@ -125,3 +125,31 @@ def calc_rank_comb_score(cards: torch.Tensor):
 
 def calc_order_score(cards: torch.Tensor):
     pass
+
+
+def deal(cards: torch.Tensor, dealt: int):
+    batch_size = cards.shape[0]
+    size = cards.shape[1]
+
+    for n in range(dealt, size):
+        if n > 1:
+            ordered, _ = torch.sort(cards[:, :n], dim=1)
+            cards[:, :n] = ordered
+
+        cards[:, n] = torch.randint(0, 52 - n, (batch_size,), device=cards.device)
+
+        for i in range(n):
+            cards[:, n] += cards[:, n] >= cards[:, i]
+
+    ordered, _ = torch.sort(cards[:, :], dim=1)
+    cards[:, :] = ordered
+
+
+def test_deal():
+    cards = torch.zeros((100, 7), dtype=torch.int)
+    deal(cards, 0)
+    print(cards)
+
+
+if __name__ == '__main__':
+    test_deal()
